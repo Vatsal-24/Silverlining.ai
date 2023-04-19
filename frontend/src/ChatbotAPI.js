b3345317f2e59e7c31ce4d5e86dddc2a7cc219eb
Vatsal-24 <vatsaldoshi11@gmail.com>
2023-04-19 15:36:37 +0530

    MVP created

--- frontend/src/ChatbotAPI.js
+++ frontend/src/ChatbotAPI.js
@@ -28,22 +28,6 @@ const API = {
       });
     });
   },
-  GetChatGPTResponse: async (message, flag) => {
-    return new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        if (
-          message === "hi" ||
-          message === "Hi" ||
-          message === "Hey" ||
-          message === "hey" ||
-          message === "Hello" ||
-          message === "hello"
-        )
-          resolve("Welcome to SilverLining.ai !");
-        else resolve(apiCall(message, flag));
-      });
-    });
-  },
 };
 
 export default API;
