c3ec61c5ac74f478655a71fb508b078a48ea6dfb
Vatsal-24 <vatsaldoshi11@gmail.com>
2023-05-07 21:57:13 +0530

    Audio input output done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,52 +2,97 @@ import React, { useState, useEffect } from "react";
 import BotMessage from "./components/BotMessage";
 import UserMessage from "./components/UserMessage";
 import Messages from "./components/Message";
-import API from "./ChatbotAPI";
 import "./styles.css";
 import Header from "./components/Header";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import { useSpeechSynthesis } from "react-speech-kit";
 
 function App() {
   const [messages, setMessages] = useState([]);
   const [text, setText] = useState("");
   const [isButtonClicked, setIsButtonCLicked] = React.useState(false);
 
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+  const { speak } = useSpeechSynthesis();
+
   const handleInputChange = (e) => {
     setText(e.target.value);
   };
 
+  const GetChatbotResponse = async (message, flag) => {
+    if (
+      message === "hi" ||
+      message === "Hi" ||
+      message === "Hey" ||
+      message === "hey" ||
+      message === "Hello" ||
+      message === "hello"
+    ) {
+      speak({ text: "Welcome to SilverLining.ai !" });
+      return "Welcome to SilverLining.ai !";
+    } else {
+      let ans = "Please try again later";
+      await fetch(`http://127.0.0.1:5000/getAnswer/${message}/${flag}`)
+        .then((response) => response.json())
+        .then((data) => {
+          ans = data[1];
+          console.log(ans);
+        });
+      speak({ text: ans });
+      return ans;
+    }
+  };
+
   const submitText = async (e) => {
-    console.log("hello");
+    let input = "";
+    let output = "";
+    transcript === "" ? (input = text) : (input = transcript);
+
     e.preventDefault();
     if (
-      text === "hi" ||
-      text === "Hi" ||
-      text === "Hey" ||
-      text === "hey" ||
-      text === "Hello" ||
-      text === "hello"
+      input === "hi" ||
+      input === "Hi" ||
+      input === "Hey" ||
+      input === "hey" ||
+      input === "Hello" ||
+      input === "hello"
     ) {
       const newMessages = messages.concat(
-        <UserMessage key={messages.length + 1} text={text} />,
+        <UserMessage key={messages.length + 1} text={input} />,
         <BotMessage
           key={messages.length + 2}
           fetchMessage={() => {
+            speak({ text: "Welcome to SilverLining.ai !" });
             return "Welcome to SilverLining.ai !";
           }}
         />
       );
       setMessages(newMessages);
       setText("");
+      resetTranscript();
     } else {
       const newMessages = messages.concat(
-        <UserMessage key={messages.length + 1} text={text} />,
+        <UserMessage key={messages.length + 1} text={input} />,
         <BotMessage
           key={messages.length + 2}
-          fetchMessage={async () => await API.GetChatbotResponse(text, "yes")}
+          fetchMessage={async () => {
+            const res = await GetChatbotResponse(input, "yes");
+            output = res;
+            return res;
+          }}
         />
       );
       setMessages(newMessages);
     }
     setText("");
+    resetTranscript();
   };
 
   useEffect(() => {
@@ -55,7 +100,7 @@ function App() {
       setMessages([
         <BotMessage
           key="0"
-          fetchMessage={async () => await API.GetChatbotResponse("hi")}
+          fetchMessage={async () => await GetChatbotResponse("hi")}
         />,
       ]);
     }
@@ -73,11 +118,12 @@ function App() {
             id="myInput"
             type="text"
             onChange={handleInputChange}
-            value={text}
+            value={text || transcript}
             placeholder="Enter your message here"
             autoFocus
           />
-          <button id="myBtn" type={"submit"}>
+
+          <button type={"submit"}>
             <svg
               version="1.1"
               xmlns="http://www.w3.org/2000/svg"
@@ -93,6 +139,7 @@ function App() {
             </svg>
           </button>
         </form>
+        <button onClick={SpeechRecognition.startListening}>Start</button>
       </div>
     </div>
   );
